7dc18402c2510f0f369d090e5f3febf8cd639e38
NoddyNZ <mpf.morris@gmail.com>
2015-07-01 13:17:22 +1200

    Both methods working. 2nd method needs tweeking.

--- MediaProcessor/Form1.Designer.cs
+++ MediaProcessor/Form1.Designer.cs
@@ -30,11 +30,14 @@
         {
             this.btnScanMedia = new System.Windows.Forms.Button();
             this.lstBox = new System.Windows.Forms.ListBox();
+            this.lblSelectedFolder = new System.Windows.Forms.Label();
+            this.btnScanFile = new System.Windows.Forms.Button();
+            this.lstBoxEpisode = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // btnScanMedia
             // 
-            this.btnScanMedia.Location = new System.Drawing.Point(12, 12);
+            this.btnScanMedia.Location = new System.Drawing.Point(12, 32);
             this.btnScanMedia.Name = "btnScanMedia";
             this.btnScanMedia.Size = new System.Drawing.Size(108, 23);
             this.btnScanMedia.TabIndex = 0;
@@ -45,21 +48,51 @@
             // lstBox
             // 
             this.lstBox.FormattingEnabled = true;
-            this.lstBox.Location = new System.Drawing.Point(473, 12);
+            this.lstBox.Location = new System.Drawing.Point(126, 12);
             this.lstBox.Name = "lstBox";
             this.lstBox.Size = new System.Drawing.Size(294, 238);
             this.lstBox.TabIndex = 1;
+            // 
+            // lblSelectedFolder
+            // 
+            this.lblSelectedFolder.AutoSize = true;
+            this.lblSelectedFolder.Location = new System.Drawing.Point(13, 13);
+            this.lblSelectedFolder.Name = "lblSelectedFolder";
+            this.lblSelectedFolder.Size = new System.Drawing.Size(0, 13);
+            this.lblSelectedFolder.TabIndex = 2;
+            // 
+            // btnScanFile
+            // 
+            this.btnScanFile.Location = new System.Drawing.Point(13, 62);
+            this.btnScanFile.Name = "btnScanFile";
+            this.btnScanFile.Size = new System.Drawing.Size(107, 23);
+            this.btnScanFile.TabIndex = 3;
+            this.btnScanFile.Text = "Scan File";
+            this.btnScanFile.UseVisualStyleBackColor = true;
+            this.btnScanFile.Click += new System.EventHandler(this.btnScanFile_Click);
+            // 
+            // lstBoxEpisode
+            // 
+            this.lstBoxEpisode.FormattingEnabled = true;
+            this.lstBoxEpisode.Location = new System.Drawing.Point(426, 12);
+            this.lstBoxEpisode.Name = "lstBoxEpisode";
+            this.lstBoxEpisode.Size = new System.Drawing.Size(294, 238);
+            this.lstBoxEpisode.TabIndex = 4;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(779, 324);
+            this.ClientSize = new System.Drawing.Size(749, 269);
+            this.Controls.Add(this.lstBoxEpisode);
+            this.Controls.Add(this.btnScanFile);
+            this.Controls.Add(this.lblSelectedFolder);
             this.Controls.Add(this.lstBox);
             this.Controls.Add(this.btnScanMedia);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -67,6 +100,9 @@
 
         private System.Windows.Forms.Button btnScanMedia;
         private System.Windows.Forms.ListBox lstBox;
+        private System.Windows.Forms.Label lblSelectedFolder;
+        private System.Windows.Forms.Button btnScanFile;
+        private System.Windows.Forms.ListBox lstBoxEpisode;
     }
 }
 
--- MediaProcessor/Form1.cs
+++ MediaProcessor/Form1.cs
@@ -1,17 +1,22 @@
-﻿using System;
+﻿using MediaProcessor.Classes;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TagLib;
 
 namespace MediaProcessor
 {
     public partial class Form1 : Form
     {
+        public string mediaFolder = "E:\\Media";
+
         public Form1()
         {
             InitializeComponent();
@@ -19,7 +24,63 @@ namespace MediaProcessor
 
         private void btnScanMedia_Click(object sender, EventArgs e)
         {
-            string mediaFolder = "E:\\Media";
+            lstBox.Items.Clear();
+            var extList = new string[] { ".mp3" };
+                        
+            var files = Directory.GetFiles(mediaFolder).Select(Path.GetFileName)
+                .Where(n => extList.Contains(System.IO.Path.GetExtension(n), StringComparer.OrdinalIgnoreCase)).ToList();
+
+            lblSelectedFolder.Text = mediaFolder;
+
+            int count = files.Count();
+            for (int i = 0; i <= count - 1; i++)
+            {
+                lstBox.Items.Add(files[i]);
+            }
+
+
+        }
+
+        private void btnScanFile_Click(object sender, EventArgs e)
+        {
+            string selectedFile = lstBox.SelectedItem.ToString();
+            string mediaFile = mediaFolder + "\\" + selectedFile;
+
+            using (FileStream fs = System.IO.File.OpenRead(mediaFile))
+            {
+                if (fs.Length >= 128)
+                {
+                    MusicID3Tag tag = new MusicID3Tag();
+                    fs.Seek(-128, SeekOrigin.End);
+                    fs.Read(tag.TAGID, 0, tag.TAGID.Length);
+                    fs.Read(tag.Title, 0, tag.Title.Length);
+                    fs.Read(tag.Artist, 0, tag.Artist.Length);
+                    fs.Read(tag.Album, 0, tag.Album.Length);
+                    fs.Read(tag.Year, 0, tag.Year.Length);
+                    fs.Read(tag.Comment, 0, tag.Comment.Length);
+                    fs.Read(tag.Genre, 0, tag.Genre.Length);
+                    string theTAGID = Encoding.Default.GetString(tag.TAGID);
+
+                    if (theTAGID.Equals("TAG"))
+                    {
+                        string Title = Encoding.Default.GetString(tag.Title);
+                        string Artist = Encoding.Default.GetString(tag.Artist);
+                        string Album = Encoding.Default.GetString(tag.Album);
+                        string Year = Encoding.Default.GetString(tag.Year);
+                        string Comment = Encoding.Default.GetString(tag.Comment);
+                        string Genre = Encoding.Default.GetString(tag.Genre);
+
+                        lstBoxEpisode.Items.Clear();
+
+                        lstBoxEpisode.Items.Add(Title);
+                        lstBoxEpisode.Items.Add(Artist);
+                        lstBoxEpisode.Items.Add(Album);
+                        lstBoxEpisode.Items.Add(Year);
+                        lstBoxEpisode.Items.Add(Comment);
+                        lstBoxEpisode.Items.Add(Genre);
+                    }
+                }
+            }
         }
     }
 }
